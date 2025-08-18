41ba356b9aef02c4f745550578f4fdbd65713bfc
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-18 11:13:18 +0545

    configure main.ts file according to requirement

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('NestApplication');
+  const app = await NestFactory.create<NestApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const PORT = process.env.PORT ?? 3000;
+  await app
+    .listen(PORT)
+    .then(() => logger.log(`App Listening to Port ${PORT}`));
 }
 bootstrap();
