2e7df26368f573c1c3480a39e89eb1adabc35624
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-18 13:51:32 +0545

    add app configuration from nest config to common module

---
+++ src/common/common.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configs from './configs';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      load: configs,
+    }),
+  ],
+  exports: [],
+})
+export class CommonModule {}
