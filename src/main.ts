ba8984c95e3f96e861364c10e0e11d50217cb1a1
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-18 13:50:43 +0545

    use config in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger('NestApplication');
+
   const app = await NestFactory.create<NestApplication>(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('app.port') ?? 3000;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,9 +19,8 @@ async function bootstrap() {
     }),
   );
 
-  const PORT = process.env.PORT ?? 3000;
   await app
-    .listen(PORT)
-    .then(() => logger.log(`App Listening to Port ${PORT}`));
+    .listen(port)
+    .then(() => logger.log(`App Listening to Port ${port}`));
 }
 bootstrap();
