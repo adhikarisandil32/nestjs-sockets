f71ffe51e765b5e0247af6dd686ce9c3a78a00b6
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-21 18:04:26 +0545

    create respources users-group for chat

---
+++ src/modules/groups/entities/group.entity.ts
@@ -0,0 +1,24 @@
+import { DBBaseEntity } from 'src/common/database/entities/base.entity';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+
+@Entity({ name: 'groups' })
+export class GroupEntity extends DBBaseEntity {
+  @ManyToOne(() => UserEntity, (user) => user.id, { nullable: false })
+  @JoinColumn({ name: 'group_admin' })
+  groupAdmin: UserEntity;
+
+  @ManyToMany(() => UserEntity, (user) => user.id, { nullable: false })
+  @JoinTable({
+    name: 'groups_users',
+    joinColumn: {
+      name: 'group_id',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'user_id',
+      referencedColumnName: 'id',
+    },
+  })
+  users: UserEntity;
+}
