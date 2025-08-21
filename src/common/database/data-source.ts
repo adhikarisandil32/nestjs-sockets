8e943f3958bb952777e2d85a3e90978a4d9d266c
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-21 18:01:27 +0545

    create a data-source file for migration purpose

---
+++ src/common/database/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource: DataSource = new DataSource({
+  type: 'postgres',
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT!,
+  entities: [__dirname + './../../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + './../../migrations/*{.ts,.js}'],
+  synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+});
