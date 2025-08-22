3c01b1fc1cd2cd2a8775e99feb6ef9a8603c09aa
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-23 03:02:02 +0545

    make changes to seed.command class

--- src/command/seed.command.ts
+++ src/command/seed.command.ts
@@ -1,14 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { Command } from 'nestjs-command';
 import { DataSource } from 'typeorm';
+import { seedUsers, seedGroups } from './seed-helper';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { GroupEntity } from 'src/modules/groups/entities/group.entity';
 
 @Injectable()
 export class SeedDatabase {
   constructor(private readonly _dataSource: DataSource) {}
 
-  @Command({ command: 'seed:db' })
+  @Command({ command: 'db:seed' })
   async create() {
-    console.log('seeding DB start');
-    console.log('seeding DB done');
+    const queryRunner = this._dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      console.log('seeding begins');
+
+      await seedUsers(queryRunner.manager.getRepository(UserEntity));
+      await seedGroups(
+        queryRunner.manager.getRepository(UserEntity),
+        queryRunner.manager.getRepository(GroupEntity),
+      );
+
+      console.log('seeding success');
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      console.log('seeding failed, rolling back');
+      await queryRunner.rollbackTransaction();
+      throw new Error(error);
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
