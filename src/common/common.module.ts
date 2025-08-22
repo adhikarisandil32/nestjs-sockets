32f3528a299808a11060e1d3f3685b4c2b849269
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-22 18:07:47 +0545

    import sperated config.module on common.module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import configs from './configs';
-import validationSchema from './configs/validation-schema';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './configs/config.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env'],
-      isGlobal: true,
-      load: configs,
-      validationSchema: validationSchema,
-      validationOptions: {
-        allowUnknown: true,
-        abortEarly: true,
-      },
-    }),
-    DatabaseModule,
-  ],
+  imports: [ConfigModule, DatabaseModule],
   exports: [],
 })
 export class CommonModule {}
