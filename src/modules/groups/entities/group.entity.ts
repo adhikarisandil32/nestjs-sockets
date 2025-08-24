326621f48d43f9b49ac20275fdde14142ca0aa50
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-25 01:13:10 +0545

    add name column to group entity

--- src/modules/groups/entities/group.entity.ts
+++ src/modules/groups/entities/group.entity.ts
@@ -1,9 +1,19 @@
 import { DBBaseEntity } from 'src/common/database/entities/base.entity';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
-import { Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+} from 'typeorm';
 
 @Entity({ name: 'groups' })
 export class GroupEntity extends DBBaseEntity {
+  @Column({ name: 'name', nullable: false, default: 'my group' })
+  name: string;
+
   @ManyToOne(() => UserEntity, (user) => user.id, { nullable: false })
   @JoinColumn({ name: 'group_admin' })
   groupAdmin: UserEntity;
