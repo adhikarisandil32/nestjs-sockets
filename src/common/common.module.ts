42640dc33cee3d3f64576a486b924a70220d016b
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-25 01:09:45 +0545

    setup pino logger module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from './configs/config.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+        },
+      },
+    }),
+  ],
   exports: [],
 })
 export class CommonModule {}
