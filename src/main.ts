5d26141e21363a2e6d42bda459616afacd78d855
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-26 01:44:25 +0545

    utilize nest process to log the memory consumption on bootstrap

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,19 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('api');
 
+  // to monitor the nestapp's process memory
+  // setInterval(() => {
+  //   const processMemory = process.memoryUsage();
+
+  //   console.log({
+  //     rss: `${Math.ceil(processMemory.rss / (1024 * 1024))} MB`,
+  //     heapTotal: `${Math.ceil(processMemory.heapTotal / (1024 * 1024))} MB`,
+  //     heapUsed: `${Math.ceil(processMemory.heapUsed / (1024 * 1024))} MB`,
+  //     external: `${Math.ceil(processMemory.external / (1024 * 1024))} MB`,
+  //     time: Date.now(),
+  //   });
+  // }, 5000);
+
   await app
     .listen(port)
     .then(() => logger.log(`App Listening to Port ${port}`));
