b6912ce846f5d90a639b0aebbb30f78c02eb9f6b
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-27 00:00:19 +0545

    provide app.config enum

--- src/common/configs/app.config.ts
+++ src/common/configs/app.config.ts
@@ -1,5 +1,10 @@
 import { registerAs } from '@nestjs/config';
 
+export enum APP_MODE {
+  DEV = 'development',
+  PROD = 'production',
+}
+
 export default registerAs('app', () => ({
   mode: process.env.NODE_ENV,
   port: isNaN(Number(process.env.PORT)) ? 3000 : parseInt(process.env.PORT!),
