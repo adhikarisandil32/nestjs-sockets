ad5605bdb3a97cfc27dc2d451a22d12da32cbcc9
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-26 18:11:00 +0545

    import memory logger middleware to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { SocketModule } from './socket/socket.module';
 import { GroupsModule } from './modules/groups/group.module';
+import { MemoryLoggerMiddleware } from './common/memory-logger/memory-logger.middleware';
 
 @Module({
   imports: [CommonModule, GroupsModule, SocketModule],
   exports: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MemoryLoggerMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
