834fc87bd4fe3128160e7f587126827fe84cc704
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-26 18:11:19 +0545

    log file if memory exceeds a threshold

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const logger = new Logger('NestApplication');
@@ -20,21 +21,38 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('api');
 
-  // to monitor the nestapp's process memory
-  // setInterval(() => {
-  //   const processMemory = process.memoryUsage();
+  if (configService.get<string>('app.mode') !== 'production') {
+    // to monitor the nestapp's process memory
+    try {
+      setInterval(() => {
+        const processMemory = process.memoryUsage();
 
-  //   console.log({
-  //     rss: `${Math.ceil(processMemory.rss / (1024 * 1024))} MB`,
-  //     heapTotal: `${Math.ceil(processMemory.heapTotal / (1024 * 1024))} MB`,
-  //     heapUsed: `${Math.ceil(processMemory.heapUsed / (1024 * 1024))} MB`,
-  //     external: `${Math.ceil(processMemory.external / (1024 * 1024))} MB`,
-  //     time: Date.now(),
-  //   });
-  // }, 5000);
+        // if memory exceeds 500MB, log the memory info
+        if (processMemory.rss > 500 * 1024 * 1024) {
+          const processMemoryInMb = {
+            datetime: Date.now(),
+            rss: `${(processMemory.rss / (1024 * 1024)).toFixed(2)} MB`,
+            heapTotal: `${(processMemory.heapTotal / (1024 * 1024)).toFixed(2)} MB`,
+            heapUsed: `${(processMemory.heapUsed / (1024 * 1024)).toFixed(2)} MB`,
+            external: `${(processMemory.external / (1024 * 1024)).toFixed(2)} MB`,
+            arrayBuffers: `${(processMemory.arrayBuffers / (1024 * 1024)).toFixed(2)} MB`,
+          };
+
+          fs.appendFile(
+            './app_memory_usage_logs.txt',
+            ', ' + JSON.stringify(processMemoryInMb),
+            () => null,
+          );
+        }
+      }, 1000);
+    } catch (error) {
+      console.log(new Error(error));
+    }
+  }
 
   await app
     .listen(port)
     .then(() => logger.log(`App Listening to Port ${port}`));
 }
+
 bootstrap();
