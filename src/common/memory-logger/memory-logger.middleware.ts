abad631198177f6212f0954a2f6630c8b98680d4
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-27 00:00:58 +0545

    create a memory logger middleware to log memory usage before and after every response on development

--- src/common/memory-logger/memory-logger.middleware.ts
+++ src/common/memory-logger/memory-logger.middleware.ts
@@ -1,45 +1,66 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NextFunction, Request, Response } from 'express';
 import * as fs from 'fs';
+import { APP_MODE } from '../configs/app.config';
 
 @Injectable()
 export class MemoryLoggerMiddleware implements NestMiddleware {
+  constructor(private configService: ConfigService) {}
+
   use(req: Request, res: Response, next: NextFunction) {
-    const startUsage = process.memoryUsage();
+    if (this.configService.get<string>('app.mode') === APP_MODE.DEV) {
+      const startUsage = process.memoryUsage();
 
-    const startUsageInMb = {
-      datetime: Date.now(),
-      rss: `${(startUsage.rss / (1024 * 1024)).toFixed(2)} MB`,
-      heapTotal: `${(startUsage.heapTotal / (1024 * 1024)).toFixed(2)} MB`,
-      heapUsed: `${(startUsage.heapUsed / (1024 * 1024)).toFixed(2)} MB`,
-      external: `${(startUsage.external / (1024 * 1024)).toFixed(2)} MB`,
-      arrayBuffers: `${(startUsage.arrayBuffers / (1024 * 1024)).toFixed(2)} MB`,
-    };
-
-    fs.appendFile(
-      './request_memory_usage_logs.txt',
-      ',' + JSON.stringify(startUsageInMb),
-      () => null,
-    );
-
-    res.on('finish', () => {
-      const endUsage = process.memoryUsage();
-
-      const endUsageInMb = {
+      const startUsageInMb = {
         datetime: Date.now(),
-        rss: `${(endUsage.rss / (1024 * 1024)).toFixed(2)} MB`,
-        heapTotal: `${(endUsage.heapTotal / (1024 * 1024)).toFixed(2)} MB`,
-        heapUsed: `${(endUsage.heapUsed / (1024 * 1024)).toFixed(2)} MB`,
-        external: `${(endUsage.external / (1024 * 1024)).toFixed(2)} MB`,
-        arrayBuffers: `${(endUsage.arrayBuffers / (1024 * 1024)).toFixed(2)} MB`,
+        rss: `${(startUsage.rss / (1024 * 1024)).toFixed(2)} MB`,
+        heapTotal: `${(startUsage.heapTotal / (1024 * 1024)).toFixed(2)} MB`,
+        heapUsed: `${(startUsage.heapUsed / (1024 * 1024)).toFixed(2)} MB`,
+        external: `${(startUsage.external / (1024 * 1024)).toFixed(2)} MB`,
+        arrayBuffers: `${(startUsage.arrayBuffers / (1024 * 1024)).toFixed(2)} MB`,
       };
 
-      fs.appendFile(
-        './request_memory_usage_logs.txt',
-        ',' + JSON.stringify(endUsageInMb),
-        () => null,
-      );
-    });
+      res.on('finish', () => {
+        const endUsage = process.memoryUsage();
+
+        const endUsageInMb = {
+          datetime: Date.now(),
+          rss: `${(endUsage.rss / (1024 * 1024)).toFixed(2)} MB`,
+          heapTotal: `${(endUsage.heapTotal / (1024 * 1024)).toFixed(2)} MB`,
+          heapUsed: `${(endUsage.heapUsed / (1024 * 1024)).toFixed(2)} MB`,
+          external: `${(endUsage.external / (1024 * 1024)).toFixed(2)} MB`,
+          arrayBuffers: `${(endUsage.arrayBuffers / (1024 * 1024)).toFixed(2)} MB`,
+        };
+
+        const filePath = './request_memory_usage_logs.txt';
+
+        if (!fs.existsSync(filePath)) {
+          fs.writeFile(
+            filePath,
+            JSON.stringify({
+              path: req.path,
+              beforeResponse: startUsageInMb,
+              afterResponse: endUsageInMb,
+            }),
+            () => null,
+          );
+
+          return;
+        }
+
+        fs.appendFile(
+          filePath,
+          ', ' +
+            JSON.stringify({
+              path: req.path,
+              beforeResponse: startUsageInMb,
+              afterResponse: endUsageInMb,
+            }),
+          () => null,
+        );
+      });
+    }
 
     next();
   }
