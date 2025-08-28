0e8b9bd63bdf650abd6acf0b1d3304292f34fa95
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-28 18:11:30 +0545

    implement router module in app

--- src/router/router.module.ts
+++ src/router/router.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RouterModule as NestRouter } from '@nestjs/core';
 import { ApiRoutesModule } from './routes/api-routes.module';
+import { SocketModule } from 'src/socket/socket.module';
 
 @Module({
   imports: [
--- src/router/routes/api-routes.module.ts
+++ src/router/routes/api-routes.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/modules/auth/auth.module';
+import { AuthController } from 'src/modules/auth/controllers/auth.controller';
 import { GroupsController } from 'src/modules/groups/controllers/groups.controller';
 import { GroupsModule } from 'src/modules/groups/group.module';
 import { UsersController } from 'src/modules/users/controllers/users.controller';
 import { UsersModule } from 'src/modules/users/users.module';
 
 @Module({
-  imports: [GroupsModule, UsersModule],
-  controllers: [GroupsController, UsersController],
+  imports: [GroupsModule, UsersModule, AuthModule],
+  controllers: [GroupsController, UsersController, AuthController],
 })
 export class ApiRoutesModule {}
