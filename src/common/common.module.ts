f3962154052172f96bfc57177c1ef087e86b53f8
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-28 10:56:19 +0545

    add response.module to common.module

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from './configs/config.module';
 import { LoggerModule } from 'nestjs-pino';
+import { ResponseModule } from './response/response.module';
 // import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     ConfigModule,
     DatabaseModule,
+    ResponseModule,
     LoggerModule.forRoot({
       pinoHttp: {
         transport: {
