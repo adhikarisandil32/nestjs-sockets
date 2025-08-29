238b75d4e2c8e9a688c94b6993f8f56cb5ee4b30
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-29 18:10:21 +0545

    update auth service and controller with the guard

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { LoginAuthDto } from '../dtos/login.auth.dto';
-import { AuthGuard } from '../decorators/auth-guard.decorator';
+import { Request as IRequest } from 'express';
+import { UserProtected } from '../decorators/auth-guard.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @UserProtected()
+  @Get('me')
+  async getMe(@Req() req: IRequest) {
+    return await this.authService.getMe(req.user as any);
+  }
 
-  @AuthGuard()
   @Post('login')
   async login(@Body() credentials: LoginAuthDto) {
     return await this.authService.login(credentials);
--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -2,14 +2,23 @@ import { UsersService } from 'src/modules/users/services/users.service';
 import { DataSource } from 'typeorm';
 import { LoginAuthDto } from '../dtos/login.auth.dto';
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
+import { ConfigService } from '@nestjs/config';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
+  private readonly secretKey: string;
+
   constructor(
     private readonly _usersService: UsersService,
     private readonly _dataSource: DataSource,
-  ) {}
+    private readonly _jwtService: JwtService,
+    private readonly _configService: ConfigService,
+  ) {
+    this.secretKey = this._configService.get<string>('jwt.secretKey')!;
+  }
 
   async login(credentials: LoginAuthDto) {
     const existingUser = await this._usersService.findOneByEmail({
@@ -29,6 +38,16 @@ export class AuthService {
       throw new NotFoundException('email or password did not match');
     }
 
-    return existingUser;
+    return {
+      user: existingUser,
+      accessToken: this._jwtService.sign(
+        { id: existingUser.id, role: existingUser.role },
+        { secret: this.secretKey },
+      ),
+    };
+  }
+
+  async getMe(user: UserEntity) {
+    return user;
   }
 }
