187ed7139797ed6ad2896e47a18a2d9c406beff3
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-29 18:09:59 +0545

    create jwt auth guard

--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -1,5 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+import { USER_ROLE } from 'src/modules/users/constants/user.constant';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    return super.canActivate(context);
+  }
+
+  handleRequest(
+    err: Error,
+    user: { id: number; role: USER_ROLE },
+    info: Error,
+  ): any {
+    try {
+      if (err || !user || !user?.id || !user?.role) {
+        throw new UnauthorizedException({
+          statusCode: HttpStatus.UNAUTHORIZED,
+          message: 'Not Authorized!',
+          _error: err ? err?.message : info?.message,
+        });
+      }
+      return user;
+    } catch (error) {
+      // console.log('Error in ', AuthJwtAccessGuard.name, ' ', error);
+      throw error;
+    }
+  }
+}
