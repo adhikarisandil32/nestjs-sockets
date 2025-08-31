7acf4774a53c452e64da1b62c4adf6ae28465e0b
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-09-01 01:20:25 +0545

    add ignore expiration false flag on jwt strategy super

--- src/modules/auth/strategy/jwt.strategy.ts
+++ src/modules/auth/strategy/jwt.strategy.ts
@@ -9,6 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get<string>('jwt.secretKey')!,
+      ignoreExpiration: false,
     });
   }
 
