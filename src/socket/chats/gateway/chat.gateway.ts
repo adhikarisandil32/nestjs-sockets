c0daadfe89ea775bf904c293d1e5c9124b39dd3c
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-09-01 18:11:08 +0545

    authenticate user based on connection

--- src/socket/chats/gateway/chat.gateway.ts
+++ src/socket/chats/gateway/chat.gateway.ts
@@ -1,4 +1,6 @@
 import { UseGuards } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import {
   ConnectedSocket,
   MessageBody,
@@ -11,18 +13,32 @@ import {
   WsException,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { WsJwtAuthGuard } from 'src/modules/auth/guards/ws-auth.guard';
+import { USER_ROLE } from 'src/modules/users/constants/user.constant';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+import { UsersService } from 'src/modules/users/services/users.service';
+// import { WsJwtAuthGuard } from 'src/modules/auth/guards/ws-auth.guard';
 
 @WebSocketGateway({ cors: true, namespace: 'socket/chat' })
-@UseGuards(WsJwtAuthGuard)
+// @UseGuards(WsJwtAuthGuard)
 export class ChatGateway
   implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
 {
   @WebSocketServer()
   server: Server;
 
-  private connectedUsers: Set<string> = new Set();
-  private connectedUsersCount: number = 0;
+  private jwtSecret: string;
+  private connectedUsers: Set<string>;
+  private connectedUsersCount: number;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+    private readonly _usersService: UsersService,
+  ) {
+    this.connectedUsers = new Set();
+    this.connectedUsersCount = 0;
+    this.jwtSecret = configService.get<string>('jwt.secretKey')!;
+  }
 
   afterInit(server: Server) {
     server.use(async (socket, next) => {
@@ -33,6 +49,23 @@ export class ChatGateway
           throw new WsException('no authorization token');
         }
 
+        const decodedToken = this.jwtService.verify<{
+          id: number;
+          role: USER_ROLE;
+        }>(token, {
+          secret: this.jwtSecret,
+        });
+
+        const associatedUser = await this._usersService.findOneById(
+          decodedToken.id,
+        );
+
+        if (!associatedUser) {
+          throw new WsException('user not available');
+        }
+
+        socket.handshake['user'] = associatedUser;
+
         next();
       } catch (error) {
         socket.disconnect();
@@ -42,14 +75,19 @@ export class ChatGateway
   }
 
   handleConnection(@ConnectedSocket() client: Socket) {
-    this.connectedUsers.add(client.id);
+    const socketUser: UserEntity = client.handshake['user'];
+
+    this.connectedUsers.add(socketUser.email);
     this.connectedUsersCount++;
     this.showConnectedClients();
   }
 
   handleDisconnect(@ConnectedSocket() client: Socket) {
     client.disconnect();
-    this.connectedUsers.delete(client.id);
+
+    const socketUser: UserEntity = client.handshake['user'];
+
+    this.connectedUsers.delete(socketUser.email);
     this.connectedUsersCount--;
     this.showConnectedClients();
   }
@@ -66,10 +104,11 @@ export class ChatGateway
     @ConnectedSocket() socket: Socket,
     @MessageBody() message: string,
   ) {
-    console.log(socket.handshake['user']);
+    const socketUser: UserEntity = socket.handshake['user'];
 
     return this.server.emit('message', {
-      clientId: socket.id,
+      socketId: socket.id,
+      userName: socketUser.name,
       message,
     });
   }
