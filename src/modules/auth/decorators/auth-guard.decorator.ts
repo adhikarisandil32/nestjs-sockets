04123ffa2fdde29e9a348bd227a9261d168a48fb
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-09-04 01:35:02 +0545

    add PutUserToRequest guard along with JwtAuthGuard guard

--- src/modules/auth/decorators/auth-guard.decorator.ts
+++ src/modules/auth/decorators/auth-guard.decorator.ts
@@ -1,8 +1,9 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../guards/auth.guard';
+import { PutUserToRequest } from '../guards/put-user.guard';
 
 export function UserProtected() {
-  const decorators: any[] = [JwtAuthGuard];
+  const decorators: any[] = [JwtAuthGuard, PutUserToRequest];
 
   return applyDecorators(UseGuards(...decorators));
 }
