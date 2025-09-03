8258b60c31db690a8b75b7b3c23af1651e58858c
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-09-04 01:38:57 +0545

    add UserProtected decorator to protect route

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserProtected } from 'src/modules/auth/decorators/auth-guard.decorator';
 import { UsersService } from '../services/users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UserProtected()
   @Get('all')
   async getAllUsers() {
     return await this.usersService.getAllUsers();
