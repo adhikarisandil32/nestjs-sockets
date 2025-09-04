de0da4e4943f23580bd9590ff185276b4628ab28
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-09-05 03:37:49 +0545

    providing one to many relationships on the contrary

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,7 +1,9 @@
 import { UserBaseEntity } from 'src/common/database/entities/user.base.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { USER_ROLE } from '../constants/user.constant';
 import { TableNames } from 'src/common/database/constants/common.constant';
+import { GroupEntity } from 'src/modules/groups/entities/group.entity';
+import { UserGroupEntity } from 'src/modules/users-groups/entities/users-groups.entity';
 
 @Entity({ name: TableNames.UsersTable })
 export class UserEntity extends UserBaseEntity {
@@ -16,4 +18,14 @@ export class UserEntity extends UserBaseEntity {
 
   @Column({ name: 'is_active', default: false })
   isActive: boolean;
+
+  @OneToMany(() => GroupEntity, (group) => group.groupAdmin, {
+    nullable: false,
+  })
+  adminsOfGroups: GroupEntity[];
+
+  @OneToMany(() => UserGroupEntity, (userGroup) => userGroup.member, {
+    nullable: false,
+  })
+  groups: UserGroupEntity[];
 }
