b2136848c01924f806ca8b331f9b1674dbd724d1
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-09-04 18:10:38 +0545

    remove many-to-many from group.entity

--- src/modules/groups/entities/group.entity.ts
+++ src/modules/groups/entities/group.entity.ts
@@ -1,14 +1,7 @@
 import { TableNames } from 'src/common/database/constants/common.constant';
 import { DBBaseEntity } from 'src/common/database/entities/base.entity';
 import { UserEntity } from 'src/modules/users/entities/user.entity';
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  JoinTable,
-  ManyToMany,
-  ManyToOne,
-} from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @Entity({ name: TableNames.GroupsTable })
 export class GroupEntity extends DBBaseEntity {
@@ -18,18 +11,4 @@ export class GroupEntity extends DBBaseEntity {
   @ManyToOne(() => UserEntity, (user) => user.id, { nullable: false })
   @JoinColumn({ name: 'group_admin' })
   groupAdmin: UserEntity;
-
-  @ManyToMany(() => UserEntity, (user) => user.id, { nullable: false })
-  @JoinTable({
-    name: TableNames.UsersGroupsLinkerTable,
-    joinColumn: {
-      name: 'group_id',
-      referencedColumnName: 'id',
-    },
-    inverseJoinColumn: {
-      name: 'user_id',
-      referencedColumnName: 'id',
-    },
-  })
-  users: UserEntity[];
 }
