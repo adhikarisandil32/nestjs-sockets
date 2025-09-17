3920b982ea248a2a23c4b9dbda8293d232d9115b
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-09-17 18:02:44 +0545

    add error filter module to common.module to catch all http exceptions

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,11 +3,13 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from './configs/config.module';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
 import { ResponseModule } from './response/response.module';
+import { ErrorFilterModule } from './error/error.module';
 // import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     ConfigModule,
+    ErrorFilterModule,
     DatabaseModule,
     ResponseModule,
     PinoLoggerModule.forRoot({
