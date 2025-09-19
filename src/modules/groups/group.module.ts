70e0956eab8dc3ab7f520d4be842e8adc0661970
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-09-19 14:46:18 +0545

    remove controllers in individual modules

--- src/modules/groups/group.module.ts
+++ src/modules/groups/group.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { GroupsController } from './controllers/groups.controller';
 import { GroupsService } from './services/group.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupEntity } from './entities/group.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([GroupEntity])],
-  controllers: [GroupsController],
   providers: [GroupsService],
   exports: [GroupsService],
 })
