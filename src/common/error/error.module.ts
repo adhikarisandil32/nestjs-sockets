5fd9cb305f7ede452509a2cb4602e2a4e84a6917
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-09-20 04:51:08 +0545

    fix import issues

--- src/common/error/error.module.ts
+++ src/common/error/error.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ErrorService } from './error.service';
 import { APP_FILTER } from '@nestjs/core';
+import { HTTPErrorService } from './error.service';
 // import { MyLogger } from '../logger.service';
 
 @Module({
   providers: [
     {
       provide: APP_FILTER,
-      useClass: ErrorService,
+      useClass: HTTPErrorService,
     },
   ],
 })
