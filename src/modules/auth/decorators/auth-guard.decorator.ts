139175f52d79e4a7826d6a0fd2a80cd0060f6f84
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-10-15 11:53:30 +0545

    apply bearer auth decorator to user-protected decorator

--- src/modules/auth/decorators/auth-guard.decorator.ts
+++ src/modules/auth/decorators/auth-guard.decorator.ts
@@ -1,9 +1,10 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../guards/auth.guard';
 import { PutUserToRequest } from '../guards/put-user.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 export function UserProtected() {
   const decorators: any[] = [JwtAuthGuard, PutUserToRequest];
 
-  return applyDecorators(UseGuards(...decorators));
+  return applyDecorators(ApiBearerAuth(), UseGuards(...decorators));
 }
