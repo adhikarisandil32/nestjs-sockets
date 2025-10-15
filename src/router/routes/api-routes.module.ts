7ddb32de38e1ee244123b59154871bb24e8d777c
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-10-15 17:59:51 +0545

    add conversation reads to api routes

--- src/router/routes/api-routes.module.ts
+++ src/router/routes/api-routes.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { AuthController } from 'src/modules/auth/controllers/auth.controller';
+import { ConversationReadController } from 'src/modules/conversation-reads/controllers/group.conversation-read.controller';
+import { ConversationReadModule } from 'src/modules/conversation-reads/conversation-read.module';
 import { ConversationController } from 'src/modules/conversations/controllers/conversation.controller';
 import { ConversationModule } from 'src/modules/conversations/conversation.module';
 import { GroupsController } from 'src/modules/groups/controllers/groups.controller';
@@ -9,12 +11,19 @@ import { UsersController } from 'src/modules/users/controllers/users.controller'
 import { UsersModule } from 'src/modules/users/users.module';
 
 @Module({
-  imports: [AuthModule, GroupsModule, UsersModule, ConversationModule],
+  imports: [
+    AuthModule,
+    GroupsModule,
+    UsersModule,
+    ConversationModule,
+    ConversationReadModule,
+  ],
   controllers: [
     AuthController,
     GroupsController,
     UsersController,
     ConversationController,
+    ConversationReadController,
   ],
 })
 export class ApiRoutesModule {}
