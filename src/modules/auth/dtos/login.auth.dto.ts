5a62c42d6f48e93c9cf44bbf0482fe6fc66541f8
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-10-15 11:53:47 +0545

    document api property

--- src/modules/auth/dtos/login.auth.dto.ts
+++ src/modules/auth/dtos/login.auth.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginAuthDto {
+  @ApiProperty({
+    example: 'admin@gmail.com',
+    type: 'string',
+  })
   @IsNotEmpty()
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    type: 'string',
+    example: 'Test@123',
+  })
   @IsNotEmpty()
   @IsString()
   password: string;
