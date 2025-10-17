b7a9ef745a36ef08f9de77358acb164d7f9fa0fc
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-10-17 17:38:19 +0545

    add last read interface

--- src/socket/chats/interfaces/chat.interface.ts
+++ src/socket/chats/interfaces/chat.interface.ts
@@ -3,3 +3,9 @@ export interface IMessage {
   groupId: number | undefined;
   receiverUserId: number | undefined;
 }
+
+export interface ILastReadUpdate {
+  requestedUserId: number;
+  groupId: number;
+  lastReadConversationId: number;
+}
