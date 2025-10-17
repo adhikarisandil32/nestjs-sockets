00fccf9d94a52ae593b0674151b9c37b69122eca
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-10-17 17:38:02 +0545

    add services and entities in conversation-read.module

--- src/modules/conversation-reads/conversation-read.module.ts
+++ src/modules/conversation-reads/conversation-read.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LastReadConversationService } from './services/conversation-read.service';
+import { ReadSingleConversationEntity } from './entities/single.conversation-read.entity';
+import { ReadGroupConversationEntity } from './entities/group.conversation-read.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ConversationReadModule])],
-  providers: [],
-  exports: [],
+  imports: [
+    TypeOrmModule.forFeature([
+      ReadSingleConversationEntity,
+      ReadGroupConversationEntity,
+    ]),
+  ],
+  providers: [LastReadConversationService],
+  exports: [LastReadConversationService],
 })
 export class ConversationReadModule {}
