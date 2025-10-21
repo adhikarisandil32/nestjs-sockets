52c51ca0730a5ef6ade1e96d5e390ccfd4a664a3
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-10-21 09:58:03 +0545

    generate migration according to necessary changes

--- src/migrations/1760960913282-user-group-join-status.ts
+++ src/migrations/1760960913282-user-group-join-status.ts
@@ -10,15 +10,9 @@ export class UserGroupJoinStatus1760960913282 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "users_groups" ADD "join_status" "public"."users_groups_join_status_enum" NOT NULL`,
     );
-    // await queryRunner.query(
-    //   `ALTER TABLE "users_groups" ADD CONSTRAINT "UQ_8bc894be6cf4f6d60a458219c2e" UNIQUE ("member_id", "group_id")`,
-    // );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // await queryRunner.query(
-    //   `ALTER TABLE "users_groups" DROP CONSTRAINT "UQ_8bc894be6cf4f6d60a458219c2e"`,
-    // );
     await queryRunner.query(
       `ALTER TABLE "users_groups" DROP COLUMN "join_status"`,
     );
