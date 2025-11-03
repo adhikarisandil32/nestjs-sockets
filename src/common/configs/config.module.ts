d2f22984c28fa7778c511fdd945692b990630906
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-11-03 17:58:00 +0545

    move all individual configs to a dedicated directory

--- src/common/configs/config.module.ts
+++ src/common/configs/config.module.ts
@@ -1,7 +1,7 @@
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import configs from '.';
-import validationSchema from './validation-schema';
+import configs from './service-configs';
+import validationSchema from './service-configs/validation-schema';
 
 @Module({
   imports: [
--- src/common/configs/jwt.config.ts
+++
@@ -1,5 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('jwt', () => ({
-  secretKey: process.env.JWT_SECRET,
-}));
