a45f2042d670935d887ec59df204e4fce271fc89
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-11-04 01:41:11 +0545

    add file entity to users.module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { FileModule } from '../files/file.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), FileModule],
   providers: [UsersService],
   exports: [UsersService],
 })
