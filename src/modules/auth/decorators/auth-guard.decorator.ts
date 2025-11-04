0ee75a95b18a0d651339f3a8eb75fd4fb973f981
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-11-04 16:52:07 +0545

    put admin guard and admin protected decorator

--- src/modules/auth/decorators/auth-guard.decorator.ts
+++ src/modules/auth/decorators/auth-guard.decorator.ts
@@ -1,10 +1,16 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../guards/auth.guard';
-import { PutUserToRequest } from '../guards/put-user.guard';
+import { PutAdminToRequest, PutUserToRequest } from '../guards/put-user.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
 
 export function UserProtected() {
   const decorators: any[] = [JwtAuthGuard, PutUserToRequest];
+
+  return applyDecorators(ApiBearerAuth(), UseGuards(...decorators));
+}
+
+export function AdminProtected() {
+  const decorators: any[] = [JwtAuthGuard, PutAdminToRequest];
 
   return applyDecorators(ApiBearerAuth(), UseGuards(...decorators));
 }
