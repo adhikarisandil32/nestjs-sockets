2b67ed307e91d83b73a09ca2c6cec70cf2680b43
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-11-04 11:01:30 +0545

    move forward with every steps of user creation i.e. profile picture update

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -4,37 +4,78 @@ import { CreateUserDto } from '../dtos/create-user.dto';
 import { USER_ROLE } from '../constants/user.constant';
 import { FileService } from 'src/modules/files/services/file.service';
 import { Folder } from 'src/modules/files/constants/folders.file-upload';
+import { FileEntity } from 'src/modules/files/entities/file.entity';
+import { DataSource } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(
     private readonly usersService: UsersService,
     private readonly fileService: FileService,
+    private readonly dataSouce: DataSource,
   ) {}
 
   @Post('create')
   async getAllUsers(@Body() createUserDto: CreateUserDto) {
     createUserDto.role = USER_ROLE.USER;
 
-    if (createUserDto.profilePictureId) {
-      const profileImage = await this.fileService.getFileInfoOrError(
-        createUserDto.profilePictureId,
-      );
+    const queryRunner = this.dataSouce.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-      if (profileImage.associationId || profileImage.associationType) {
-        throw new ConflictException('image already used');
-      }
-    }
-
-    const newlyCreatedUser = await this.usersService.createUser(createUserDto);
-
-    if (createUserDto.profilePictureId) {
-      await this.fileService.updateFileInfo(createUserDto.profilePictureId, {
-        associationId: newlyCreatedUser.id,
-        associationType: Folder.Profile,
+    try {
+      const existingUser = await this.usersService.findOneByEmail({
+        email: createUserDto.email,
       });
-    }
 
-    return newlyCreatedUser;
+      if (existingUser) {
+        throw new ConflictException('email already taken');
+      }
+
+      const userRepo = queryRunner.manager.getRepository(UserEntity);
+      const newlyCreatedUser = userRepo.create(createUserDto);
+
+      await userRepo.save(newlyCreatedUser);
+
+      let profilePicture: FileEntity | null = null;
+
+      if (createUserDto.profilePictureId) {
+        const profileImage = await this.fileService.getFileInfoOrError(
+          createUserDto.profilePictureId,
+        );
+
+        if (profileImage.associationId || profileImage.associationType) {
+          throw new ConflictException('image already used');
+        }
+
+        profilePicture = await queryRunner.manager
+          .getRepository(FileEntity)
+          .save({
+            id: createUserDto.profilePictureId,
+            associationId: newlyCreatedUser.id,
+            associationType: Folder.Profile,
+          });
+      }
+
+      await queryRunner.commitTransaction();
+
+      // profilePicture = await this.fileService.getFileInfoOrError(
+      //   createUserDto.profilePictureId,
+      // );
+      newlyCreatedUser['profilePicture'] =
+        await this.fileService.getFileInfoOrError(
+          createUserDto.profilePictureId,
+        );
+
+      return newlyCreatedUser;
+    } catch (error) {
+      console.log(error);
+      await queryRunner.rollbackTransaction();
+
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
