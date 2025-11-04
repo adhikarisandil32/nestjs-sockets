e92bb3a2d0437408b1247be96ea0ba3fcca3e021
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-11-04 16:55:15 +0545

    in auth.service.getMe, provide also profile picture

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
-import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategy/jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { FileModule } from '../files/file.module';
 
 @Module({
-  imports: [UsersModule, PassportModule, JwtModule],
+  imports: [UsersModule, PassportModule, JwtModule, FileModule],
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
