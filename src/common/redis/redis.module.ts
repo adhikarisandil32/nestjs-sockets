caaf9c54767ad56efe2128abcaf52318ecbcf1e9
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-12-05 18:02:09 +0545

    redis client connection configuration

--- src/common/redis/redis.module.ts
+++ src/common/redis/redis.module.ts
@@ -1,14 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { REDIS_CLIENT_CONNECTION } from './redis.constant';
 import { createClient } from 'redis';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [],
   providers: [
     {
       provide: REDIS_CLIENT_CONNECTION,
-      useFactory: async () => {
-        const client = createClient({});
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const client = createClient({
+          password: configService.get<string>('redis.password'),
+          socket: {
+            host: configService.get<string>('redis.host'),
+            port: configService.get<number>('redis.port'),
+          },
+        });
+
+        const logger = new Logger(RedisModule.name);
+
+        client.on('error', (err) => {
+          logger.error('cannot connect redis');
+          console.log(err);
+        });
+
+        client.on('connect', () => logger.log('redis connection success'));
 
         await client.connect();
         return client;
