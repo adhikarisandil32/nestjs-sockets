a24131a7912cb4e6fe7e8c448962a01b0eed0852
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-12-05 18:01:39 +0545

    add methods to redis.service

--- src/common/redis/services/redis.service.ts
+++ src/common/redis/services/redis.service.ts
@@ -8,16 +8,55 @@ export class RedisService {
     @Inject(REDIS_CLIENT_CONNECTION) private readonly redis: RedisClientType,
   ) {}
 
-  async getValue(key: string) {
+  async get(key: string) {
     const value = await this.redis.get(key);
-
     return value;
   }
 
-  async setValue(key: string, value: any) {
-    console.log({ key, value });
-    const result = await this.redis.set(key, value);
+  async set(key: string, value: any, options?: Record<string, any>) {
+    await this.redis.set(key, value, options);
+  }
+
+  async delete(key: string) {
+    if (await this.keyExists(key)) {
+      await this.redis.del(key);
+    }
+  }
+
+  async keyExists(key: string) {
+    return !!(await this.redis.get(key));
+  }
+
+  // returns the value of the field in the key
+  async hGet(key: string, field: string) {
+    const value = await this.redis.hGet(key, field);
+    return value;
+  }
+
+  async hSet(
+    key: string,
+    fields: Record<string, string | number>,
+    ttl?: number | null,
+  ) {
+    const result = await this.redis.hSet(key, fields);
+
+    if (ttl) {
+      await this.redis.expire(key, ttl);
+    }
 
     return result;
+  }
+
+  // returns all value of the fields in the key
+  async hmGet(key: string, fields: string[]) {
+    const results = await this.redis.hmGet(key, fields);
+    return results.map((result) => result || null);
+  }
+
+  // deletes the individual field of the key
+  async hDelete(key: string, field: string) {
+    if (await this.keyExists(key)) {
+      await this.redis.hDel(key, field);
+    }
   }
 }
